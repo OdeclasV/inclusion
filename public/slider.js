29cf3180ccb16d3c9a6418a37f5a421312eafb44
Victor Salcedo <victorsalc2@gmail.com>
2016-11-13 21:11:27 -0500

    adding styles, footer, and form

---
+++ public/slider.js
@@ -0,0 +1,22 @@
+	//getting slider and slider images
+	var gallery = document.querySelector('.js-gallery')
+	var galleryItems = document.querySelectorAll('.js-gallery-item')
+
+	var slideCount = galleryItems.length
+	//getting the witdt of a single slider
+	var slideWidth = galleryItems[0].getBoundingClientRect().width
+
+	var slideInterval = setInterval(transitionSlide, 3000)
+
+	var currentSlide = 1
+
+	//getting slide transition to work 
+	function transitionSlide() {
+		if (currentSlide < slideCount) {
+			gallery.style.transform = "translateX(-" + slideWidth * currentSlide + "px)"
+			currentSlide++
+		} else {
+			gallery.style.transform = "translateX(0)"
+			 currentSlide = 1
+		}
+	}
